ea0b4f94913d5d017ec68ece1a98240c837b1dda
Clesyo_Silva <clesyo.silva@gmail.com>
2021-03-09 06:35:09 -0300

    Controle de acesso, implementando roles e permissions

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use Illuminate\Http\Request;
 
 class PermissionController extends Controller
 {
     //
+    public function index()
+    {
+        $pages = ['Permissões'];
+        return view('',compact('pages'));
+    }
+
+    public function create()
+    {
+        $pages = ['all_permissions'=>'Permissões','Add Permissão'];
+        return view('',compact('pages'));
+    }
+
+    public function store(Request $request)
+    {
+        $notification = null;
+        $permission = new Permission();
+
+        $permission = $permission::create($request->all());
+
+        if ($permission) {
+            $notification = [
+                'message' => 'Função incluída com sucesso.',
+                'title' => 'Mensagem',
+                'icon' => 'success',
+            ];
+        }else{
+            $notification = [
+                'message' => 'Ops! Algo deu errado, tente novamente.',
+                'title' => 'Mensagem',
+                'icon' => 'success',
+            ];
+        }
+        return redirect('all_roles')->with($notification);
+    }
 }
--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    //
+    public function index()
+    {
+        $pages = ['Funções'];
+        return view('',compact('pages'));
+    }
+
+    public function create()
+    {
+        $pages = ['all_roles'=>'Funções', 'Add Função'];
+        return view('',compact('pages'));
+    }
+
+    public function store(Request $request)
+    {
+        $notification = null;
+        $role = new Role();
+
+        $role = $role::create($request->all());
+
+        if ($role) {
+            $notification = [
+                'message' => 'Função incluída com sucesso.',
+                'title' => 'Mensagem',
+                'icon' => 'success',
+            ];
+        }else{
+            $notification = [
+                'message' => 'Ops! Algo deu errado, tente novamente.',
+                'title' => 'Mensagem',
+                'icon' => 'success',
+            ];
+        }
+        return redirect('all_roles')->with($notification);
+    }
 }
